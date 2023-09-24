6b479b3f1ccd0925f89ab619f116a408a4eb3633
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-09-24 20:37:35 +0900

    hi

--- assignment/src/main/java/com/seminar/assignment/domain/item/Item.java
+++ assignment/src/main/java/com/seminar/assignment/domain/item/Item.java
@@ -1,12 +1,10 @@
 package com.seminar.assignment.domain.item;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Item {
 
     private Long id;
